e8e2e58fb02184f55dba35d20dd9956b01f29346
mskorupski <milosz.skorupski@alten.com>
2024-08-23 16:09:09 +0200

    11 change

--- src/main/java/Super/PlanetTest.java
+++ src/main/java/Super/PlanetTest.java
@@ -7,8 +7,9 @@ public class PlanetTest {
     @Test
     public void test() {
         Planet earth = new Planet( "earth" );
-        Planet mars = new Planet( "mars" , "687" , "3389.5f" , "false" );
+        Planet mars = new Planet( "mars" , 687 , 3389.5f , false );
         mars.getOrbitalPeriod();
+        earth.getOrbitalPeriod();
     }
 
 

4fa5e8d74a7922aed9f05a184714af54a549ee6a
Begginer617 <75578722+Begginer617@users.noreply.github.com>
2024-09-03 23:14:05 +0200

    another one

--- src/main/java/AbstractClasesAndInterfaces/User.java
+++ src/main/java/AbstractClasesAndInterfaces/User.java
@@ -10,7 +10,7 @@ public class User {
 void login(String username, String password){
     if (username.equals(this.username)  && password.equals(this.password)) {
         System.out.println("Logging user");
-    }
+    }else
 
 }
 

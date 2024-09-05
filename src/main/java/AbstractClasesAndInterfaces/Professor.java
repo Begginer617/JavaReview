af2be395fed7ed0e1ab64bbc89511c940a82f29e
Begginer617 <75578722+Begginer617@users.noreply.github.com>
2024-09-05 17:55:27 +0200

    interface

--- src/main/java/AbstractClasesAndInterfaces/Professor.java
+++ src/main/java/AbstractClasesAndInterfaces/Professor.java
@@ -1,25 +1,33 @@
 package AbstractClasesAndInterfaces;
 
-public class Professor extends User implements Employee{
+public class Professor extends User implements Employee {
 
-    Professor (String username, String password){
-        super(username,password);
+    Professor(String username, String password) {
+        super(username, password);
 
     }
 
-    void giveNotes(){
+    void giveNotes() {
         System.out.println("Give Marks");
     }
-    void cancelClass(){
+
+    void cancelClass() {
         System.out.println("Lesson canceled");
     }
 
     @Override
-    void sendMessage(String message){
+    void sendMessage(String message) {
         System.out.println("Sending text message to students");
         System.out.println(message);
     }
 
+    void takeSickLeave() {
+        System.out.println("Sick leave confirmation has been sent by Professor ");
+    }
+
+    void takeVacation(int days) {
+        System.out.println("Holiday leave approval has been sent. " + "Length of holiday leave" + days + "days");
+    }
 
 
 }
